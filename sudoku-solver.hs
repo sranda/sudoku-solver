815b2d35be5e884fed9579569db07bc6ccdc4650
Martin Kovacs <kovacs@kajot.cz>
2018-02-19 16:23:34 +0100

    issue added support for sorting by individual Field accessors

--- sudoku-solver.hs
+++ sudoku-solver.hs
@@ -1,11 +1,25 @@
 import Prelude
 import System.Environment
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 data Field a = Field { r :: Int -- row index
                      , c :: Int -- col index
                      , b :: Int -- box index
                      , d :: [a] -- data
                      } deriving(Show)
+
+sortAscendingByRow :: [Field a] -> [Field a]
+sortAscendingByRow = sortBy (comparing r)
+
+sortAscendingByCol :: [Field a] -> [Field a]
+sortAscendingByCol = sortBy (comparing c)
+
+sortAscendingByBox :: [Field a] -> [Field a]
+sortAscendingByBox = sortBy (comparing b)
+
+sortAscendingByDataSize :: [Field a] -> [Field a]
+sortAscendingByDataSize = sortBy (comparing (\x -> (length . d) x))
 
 hasClosure x = (==) ((length x) - 1) . length . filter (==x)
 
