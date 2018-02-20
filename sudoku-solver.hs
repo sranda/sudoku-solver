1a40639d0cedc79c4baddb59bb1957442decb33a
Martin Kovacs <kovacs@kajot.cz>
2018-02-20 12:41:22 +0100

    issue show implementation, and sort update

--- sudoku-solver.hs
+++ sudoku-solver.hs
@@ -7,29 +7,42 @@ data Field a = Field { r :: Int -- row index
                      , c :: Int -- col index
                      , b :: Int -- box index
                      , d :: [a] -- data
-                     } deriving(Show)
+                     }
 
-sortAscendingByRow :: [Field a] -> [Field a]
-sortAscendingByRow = sortBy (comparing r)
+type Board a = [Field a]
 
-sortAscendingByCol :: [Field a] -> [Field a]
-sortAscendingByCol = sortBy (comparing c)
+sortByPosition :: [Field a] -> [Field a]
+sortByPosition = sortBy (comparing r) . sortBy (comparing c)
 
-sortAscendingByBox :: [Field a] -> [Field a]
-sortAscendingByBox = sortBy (comparing b)
+sortByDataSize :: [Field a] -> [Field a]
+sortByDataSize = sortBy (comparing (\x -> (length . d) x))
 
-sortAscendingByDataSize :: [Field a] -> [Field a]
-sortAscendingByDataSize = sortBy (comparing (\x -> (length . d) x))
+--sortAscendingByRow :: [Field a] -> [Field a]
+--sortAscendingByRow = sortBy (comparing r)
 
-hasClosure x = (==) ((length x) - 1) . length . filter (==x)
+--sortAscendingByCol :: [Field a] -> [Field a]
+--sortAscendingByCol = sortBy (comparing c)
 
-exclude x = filter (`notElem` x)
+--sortAscendingByBox :: [Field a] -> [Field a]
+--sortAscendingByBox = sortBy (comparing b)
+
+showsBoard :: (Show a) => Board a -> ShowS
+showsBoard = shows . map d . sortByPosition
+
+-- TODO
+--readsBoard :: (Read a) => ReadS (Board a)
+--readsBoard =
+
+--hasClosure x = (==) ((length x) - 1) . length . filter (== x)
+hasClosure x = (==) (length x) . length . filter (== x)
+
+excludeClosure x y = if x == y then y else filter (`notElem` x) y
 
 excludeClosures :: (Eq a) => [[a]] -> [[a]]
 excludeClosures [] = []
 excludeClosures (x:xs) = x :
                        if hasClosure x xs
-                       then excludeClosures (map (exclude x) xs)
+                       then excludeClosures (map (excludeClosure x) xs)
                        else excludeClosures xs
 
 --main = do
