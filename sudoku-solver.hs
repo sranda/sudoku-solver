addaac60fdbb2a565dcc4e5af03879a339e46f7e
sranda <sranda@github.com>
2018-02-24 23:48:14 +0100

    a minor revamp to sorting and selecting permutations

--- sudoku-solver.hs
+++ sudoku-solver.hs
@@ -3,6 +3,26 @@ import System.Environment
 import Data.List (sortBy, groupBy)
 import Data.Ord (comparing)
 
+hasClosure :: (Foldable t, Eq (t a)) => t a -> [t a] -> Bool
+hasClosure x xs = if xLength < xClosures
+                  then error ("Closure size exceeds field size.")
+                  else xLength == xClosures
+                  where (xLength, xClosures) = (length x, (length . filter (== x)) xs)
+
+excludeClosure :: (Eq a) => [a] -> [[a]] -> [[a]]
+excludeClosure _ [] = []
+excludeClosure x (y:ys) = (if x == y then y else filter (`notElem` x) y) : (excludeClosure x ys)
+
+_excludeClosures :: Eq a => [[a]] -> [[a]] -> [[a]]
+_excludeClosures [] ys = ys
+_excludeClosures (x:xs) ys = if hasClosure x ys && ys /= zs
+                             then _excludeClosures zs zs
+                             else _excludeClosures xs ys
+                             where zs = excludeClosure x ys
+
+excludeClosures :: (Eq a) => [[a]] -> [[a]]
+excludeClosures xs = _excludeClosures xs xs
+
 data Field a = Field { r :: Int -- row index
                      , c :: Int -- col index
                      , b :: Int -- box index
@@ -23,53 +43,15 @@ instance Read a => Read (Field a) where
 
 type Board a = [Field a]
 
-sortByRowIndex :: Board a -> Board a
-sortByRowIndex = sortBy (comparing r)
-
-sortByColIndex :: Board a -> Board a
-sortByColIndex = sortBy (comparing c)
-
-sortByBoxIndex :: Board a -> Board a
-sortByBoxIndex = sortBy (comparing b)
-
-sortByPosition :: Board a -> Board a
-sortByPosition = sortBy (comparing r) . sortBy (comparing c)
-
-hasClosure :: (Foldable t, Eq (t a)) => t a -> [t a] -> Bool
-hasClosure x xs = if xLength < xClosures
-                  then error ("Closure size exceeds field size.")
-                  else xLength == xClosures
-                  where (xLength, xClosures) = (length x, length $ filter (== x) xs)
-
-excludeClosure :: (Eq a) => [a] -> [[a]] -> [[a]]
-excludeClosure _ [] = []
-excludeClosure x (y:ys) = (if x == y then y else filter (`notElem` x) y) : (excludeClosure x ys)
-
-_excludeClosures :: Eq a => [[a]] -> [[a]] -> [[a]]
-_excludeClosures [] ys = ys
-_excludeClosures (x:xs) ys = if hasClosure x ys && ys /= zs
-                             then _excludeClosures zs zs
-                             else _excludeClosures xs ys
-                             where zs = excludeClosure x ys
-
-excludeClosures :: (Eq a) => [[a]] -> [[a]]
-excludeClosures xs = _excludeClosures xs xs
-
 excludeClosuresInFields :: Eq a => [Field a] -> [Field a]
 excludeClosuresInFields fs = zipWith (\f df -> Field {r = r f, c = c f, b = b f, d = df}) fs dfs
                              where dfs = (excludeClosures . map d) fs
 
-solvePermutations :: (Eq b, Eq a) => (Field a -> b) -> [Field a] -> [Field a]
-solvePermutations f = concat . map excludeClosuresInFields . groupBy (\f1 f2 -> f f1 == f f2)
+selectPermutations :: Ord b => (Field a -> b) -> Board a -> [[Field a]]
+selectPermutations accessor = groupBy (\f1 f2 -> accessor f1 == accessor f2) . sortBy (comparing accessor)
 
-solutionStepRow :: (Eq a) => Board a -> Board a
-solutionStepRow = solvePermutations r . sortByRowIndex
-
-solutionStepCol :: (Eq a) => Board a -> Board a
-solutionStepCol = solvePermutations c . sortByColIndex
-
-solutionStepBox :: (Eq a) => Board a -> Board a
-solutionStepBox = solvePermutations b . sortByBoxIndex
+solvePermutations :: (Ord b, Eq b, Eq a) => (Field a -> b) -> [Field a] -> [Field a]
+solvePermutations accessor = concat . map excludeClosuresInFields . selectPermutations accessor
 
 -- Beware! Here be the lair of bad ugly monads. --
 
@@ -79,12 +61,14 @@ solve board = if board == newBoard
                    --putStr newBoardC
                    --putStr newBoard
                    solve newBoard
-              where newBoard  = solutionStepBox newBoardC
-                    newBoardC = solutionStepCol newBoardR
-                    newBoardR = solutionStepRow board
+              where newBoardB = solvePermutations b board
+                    newBoardC = solvePermutations c newBoardB
+                    newBoardR = solvePermutations r newBoardC
+                    newBoard = newBoardR
 
 main = do
     args <- getArgs
     boardString <- readFile $ head args
-    --board = (read boardString)::(Board Int)
-    putStr boardString
+    let board = (read boardString)::(Board Int)
+    putStrLn $ show $ solve board
+
